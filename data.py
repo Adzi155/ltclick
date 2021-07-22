7578cfa040ae361a29f481dc3f5813b2887befea
Adzi155 <86840227+Adzi155@users.noreply.github.com>
2021-07-22 09:00:08 +0800

    Add files via upload

---
+++ data.py
@@ -0,0 +1,11 @@
+user agent = ""
+linkusername = "@Litecoin_click_bot"
+kode = "LTC"
+Example :  DOGE, LTC, ZEC, BTC, BCH
+
+#1. cara user agent di browser kepunyaan anda
+#2. pilih linkusername bot mengikut coin yang ingin di gunakan 
+#3. masukan kode contoh LTC
+#masukan perintah untuk menjalankan $python ltclick.py +60xxxxxxxx
+#jika error hapuskan file di folder session
+#jika terhapus anda akan disuruh masukkan kode OTP
---
+++ run.sh
@@ -0,0 +1,2 @@
+#if you use & it will run every number use & at the same time until end or limit it using &&
+python main.py +6015xxxx 
